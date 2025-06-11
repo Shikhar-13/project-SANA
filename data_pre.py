f9e6dd8aaa1b7f048572dd1701815f222debce33
Shikhar-13 <shikharguptaoct13@gmail.com>
2025-06-11 19:27:36 +0530

    trial commit

--- data_pre.py
+++ data_pre.py
@@ -1,2 +1,9 @@
 from pdfminer.high_level import extract_text
 text = extract_text('')
+arr = text.split('\n')
+def preprocess_text(text):
+    # Remove leading and trailing whitespace
+    text = text.strip()
+    # Remove multiple spaces
+    text = ' '.join(text.split())
+    return text
